7544d872d4a750aad81d07f8d546f5597d8491da
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-11-05 14:35:36 +0200

    Написав функцію додовання в локал сторейдж контактів та їх зберігання

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,14 @@ import FilterContacts from '../FilterContacts/FilterContacts';
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 // імпорт стилів
-import {AppContainer,H2} from './App.styled'
+import { AppContainer, H2 } from './App.styled';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -51,12 +51,29 @@ class App extends Component {
     );
   };
 
+  // добавлення контактів в локал сторедж
+  componentDidUpdate(prevProps, prevState) {
+    // перевірка поля на не рівність
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  // сейтить ізначальний state данних
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if(parsedContacts){
+      this.setState({contacts: parsedContacts});
+    }
+  }
+
   render() {
     const findContacts = this.getFindContacts();
     return (
       <>
         <AppContainer>
-        <H2>Contacts</H2>
+          <H2>Contacts</H2>
           <FormBook onSubmit={this.addContact} />
           <H2>Contacts list</H2>
           <FilterContacts
